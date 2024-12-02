82f95fbbb610b7eb9c23c7e6c78dd817ae4f5120
cpmiranda-sys <charlemagnep.miranda@gmail.com>
2024-12-02 21:55:03 +0800

    UPDATED

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function toggleDropdown() {
-    const dropdown = document.getElementById("dropdown");
-    dropdown.style.display = (dropdown.style.display === "block") ? "none" : "block";
+// Function to toggle popups
+function togglePopup(popupId) {
+    document.getElementById(popupId).classList.toggle("active");
+}
+
+// Function to close the popup
+function closePopup(popupId) {
+    document.getElementById(popupId).classList.remove("active");
 }
